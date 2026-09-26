dd66dd6723f9cf616877431de1fa7c256668ed49
Nitesh Kumar <devniteshkumar96@gmail.com>
2026-09-26 19:53:51 +0530

    Fix postinstall script to use absolute path for nixos directory

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,11 +2,10 @@
 set -euo pipefail
 
 git clone https://github.com/devniteshkumar/oscript.git oscript
-mkdir -p nixos
+mkdir -p ~/nixos
 cp -r ~/oscript/post-install/. ~/nixos/
 rm -rf oscript
-
-cd nixos
+cd ~/nixos
 cp /etc/nixos/hardware-configuration.nix .
 nix flake check
 sudo nixos-rebuild dry-run --flake .#nixos
